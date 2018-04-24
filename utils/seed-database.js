b6ab9df7089df7da2929065cbd89a1342ed01de3
ssheaks <sarah.sheaks@gmail.com>
2018-04-24 16:50:34 -0700

    update seed-database for brew schema

--- utils/seed-database.js
+++ utils/seed-database.js
@@ -5,13 +5,17 @@ const mongoose = require('mongoose');
 const { DATABASE_URL } = require('../config');
 const User = require('../models/user');
 const seedUsers = require('../db/seed/users');
+const Brew = require('../models/brew');
+const seedBrews = require('../db/seed/brews');
 
 mongoose.connect(DATABASE_URL)
   .then(() => mongoose.connection.db.dropDatabase())
   .then(() => {
     return Promise.all([
-      User.insertMany(seedUsers),
-      User.createIndexes(),
+      // User.insertMany(seedUsers),
+      // User.createIndexes(),
+      Brew.insertMany(seedBrews),
+      Brew.createIndexes()
     ]);
   })
   .then(() => mongoose.disconnect())
