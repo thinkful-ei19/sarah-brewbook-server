754d42eb98394cc510d603a8e5f360670d6798c5
ssheaks <sarah.sheaks@gmail.com>
2018-04-25 13:20:03 -0700

    add auth.js route

--- auth/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-//import router and strategies and export
-
-// const {router} = require('./router');
-// const {localStrategy, jwtStrategy} = require('./strategies');
-
-// module.exports = {router, localStrategy, jwtStrategy};
--- passport/local.js
+++ passport/local.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 const { Strategy: LocalStrategy } = require('passport-local');
 
